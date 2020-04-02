7e619ba974ebde427e9469c0bb14e73a2a0f6e02
Vladimir <vladimir.suragin@gmail.com>
2020-04-02 21:38:14 +0300

    create

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,28 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="app" class="container">
+    <ImageUpload />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+const ImageUpload = () => import('./components/ImageUpload');
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    ImageUpload,
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style scoped>
+.container {
+    padding: 0;
+    width: 100%;
+    height: 100vh;
+    margin: 0 auto;
+    display: flex;
+    justify-content: center;
+    align-items: center;
 }
 </style>
